ea4e06187ef7ff577e9ad0116ea7418fa209d401
TheAnachronism <Tobias.Groeli@Outlook.com>
2020-10-22 20:52:03 +0200

    Added the example testing project and the .models library.

---
+++ example/APITesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace APITesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
